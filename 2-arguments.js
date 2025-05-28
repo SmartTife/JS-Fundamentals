271ece62e7ff5d970aa1321476623e24b7810f17
SmartTife <ayoadeiradatboluwatife@gmail.com>
2025-05-29 00:49:01 +0100

    Added an argument file

---
+++ 2-arguments.js
@@ -0,0 +1,16 @@
+const args = process.argv.slice(2);
+const argCount = args.length;
+
+if (argCount === 0) {
+  console.log("No argument");
+} 
+else if (argCount === 1) {
+  console.log("Argument found");
+} 
+else {
+  console.log("Arguments found");
+}
+
+
+
+
