b18b27694184569e99f6510602d2aab0ad1f74e8
SmartTife <ayoadeiradatboluwatife@gmail.com>
2025-05-29 02:08:24 +0100

    created two arguments script

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+const statement = function(firstArg, secondArg){
+console.log(`${firstArg} is ${secondArg}`);
+};
+statement('c', 'cool');
+statement('c', 'undefined');
+statement('undefined', 'undefined');
+   
